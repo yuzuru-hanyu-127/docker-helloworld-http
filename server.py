3a6fceb7e934031ae49225a56878b47a9954644a
Erin Swenson-Healey <l@s3r.me>
2018-01-02 16:20:29 -0800

    Send correct header + add file contents

--- server.py
+++ server.py
@@ -9,16 +9,16 @@ class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         with open('./index.html', 'rb') as f:
             self.send_response(200)
+            self.send_header('Content-Type', 'text/html; charset=utf-8')
             self.end_headers()
             self.wfile.write(f.read())
-            return
 
     def do_HEAD(self):
         with open('./index.html', 'rb') as f:
             self.send_response(200)
+            self.send_header('Content-Type', 'text/html; charset=utf-8')
             self.end_headers()
             self.wfile.write(f.read())
-            return
 
 httpd = socketserver.TCPServer(("", int(PORT)), CustomRequestHandler)
 print("Python web server listening on port {}...".format(PORT))
